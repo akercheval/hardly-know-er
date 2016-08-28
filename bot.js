b7c8cdf3bbbac103c6a239e8e3a996c3fc975621
Adam Kercheval <arkercheval@gmail.com>
2016-08-28 01:50:05 -0300

    yay

--- bot.js
+++ bot.js
@@ -2,12 +2,23 @@ var HTTPS = require('https');
 var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
+var offending_word;
+var fullstring;
+var know_er = "? I hardly know 'er!";
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /\B(er)\b/;
+      botRegex = /\B(er|ER|eR|Er)\b(?!([?] I hardly know 'er!))/
 
   if(request.text && botRegex.test(request.text)) {
+    var words = request.text.split(" ");
+    for (i = 0; i < words.length; i++) {
+        if (botRegex.test(words[i])) {
+                offending_word = words[i].replace(/[^0-9a-z]/gi, '');
+                offending_word = offending_word.replace(/[^0-9a-z]/gi, '');
+                break;
+        }
+    }
     this.res.writeHead(200);
     postMessage();
     this.res.end();
@@ -29,10 +40,12 @@ function postMessage() {
     method: 'POST'
   };
 
+    fullstring = offending_word.concat(know_er);
+
   body = {
     "bot_id" : botID,
-    "text" : "i hardly know 'er!"
-    };
+    "text" : fullstring
+   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
